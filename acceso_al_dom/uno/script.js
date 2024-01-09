78bb9628caf676b68b1ede27c036320380502365
laura1513 <laurajorques2003@gmail.com>
2024-01-09 13:06:48 +0100

    tema 4, act 2, parte 1/5

--- acceso_al_dom/uno/script.js
+++ acceso_al_dom/uno/script.js
@@ -1,28 +1,44 @@
 function eliminarCompleta() {
+
     $("ul").hide();
+
 }
 function restaurar() {
+
     $("ul").show();
+
 }
 function anyadirFinal() {
+
     $("ul").append('<li>Item 0</li>');
+
 }
 function anyadirPrincipio() {
+
     $("ul").prepend("<li>Item 0</li>")
+
 }
 function eliminarUltimo(){
+
     $("li:last").remove();
+
 }
 function eliminarPrimero(){
+
     $("li:first").remove();
+
 }
 function eliminarPrimeroSegundo() {
+
     elements = $("li");
     elements[0].remove();
     elements[1].remove();
+
 }
 function eliminarDosUltimos() {
+
     elements = $("li");
     elements[elements.length-1].remove();
     elements[elements.length-2].remove();
+
 }
