0b1b27d6ee80c94c12892510c83da1f318dfb666
laura1513 <laurajorques2003@gmail.com>
2024-01-09 13:21:17 +0100

    tema 4, act 2, parte 2/5

--- acceso_al_dom/dos/script.js
+++ acceso_al_dom/dos/script.js
@@ -2,7 +2,7 @@ function imageSelected() {
     $("img").click( function () {
         $("img").css("border-width", "0");
         $("img").css("opacity", 0.4);
-        $(this).css('border', "solid 2px red");
+        $(this).css('border', "solid 3px pink");
         $(this).css('opacity', 1);
     });
 }
