ba4c43cb19dd97a0896744d98d02a3a779fb357e
laura1513 <laurajorques2003@gmail.com>
2024-01-11 14:03:09 +0100

    tema 4, act 2, parte 4/5, sin acabar

---
+++ acceso_al_dom/cuatro/script.js
@@ -0,0 +1,11 @@
+function mostrar() {
+
+}
+function ocultar() {
+    $("img").css.opacity(0.1);
+}
+$(document).ready(function () {
+    $("#ocultar").click()(function (e) {
+        $("img")
+    })
+})
