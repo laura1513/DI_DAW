1726a600f337255578710243b8a0eaaa54d456db
laura1513 <laurajorques2003@gmail.com>
2024-01-16 09:43:12 +0100

    tema 4, act 2, parte 4/5

--- acceso_al_dom/cuatro/script.js
+++ acceso_al_dom/cuatro/script.js
@@ -1,11 +1,19 @@
-function mostrar() {
+$(document).ready(function(){
+    $("#ocultar").click(function(){
+        $("img").fadeTo(0, 0.1);
+    });
+    $("img").click(function(){
+        $(this).fadeTo(0, 0.1);
+    });
+    $("#mostrar").click(function () {
 
-}
-function ocultar() {
-    $("img").css.opacity(0.1);
-}
-$(document).ready(function () {
-    $("#ocultar").click()(function (e) {
-        $("img")
-    })
-})
+        $("#img1").fadeTo(0, 1);
+        $("#img2").fadeTo(1000, 1);
+        $("#img3").fadeTo(2000, 1);
+        $("#img4").fadeTo(3000, 1);
+        $("#img5").fadeTo(4000, 1);
+        $("#img6").fadeTo(5000, 1);
+        $("#img7").fadeTo(6000, 1);
+        $("#img8").fadeTo(7000, 1);
+    });
+});
