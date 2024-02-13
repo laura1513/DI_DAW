8ef17cd4adb97cda79247b326fd70ef499867ff9
Laura Jorques <laurajorques2003@gmail.com>
2024-02-13 12:20:09 +0100

    y para acabar con node, acabado

---
+++ y_para_acabar_con_node/node2.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer(function (req, res) {
+    try {
+        const contenido = fs.readFileSync('index.html', 'utf8');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(contenido);
+    } catch (error) {
+        console.error("Error al leer el archivo.txt:", error);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.write('Error interno del servidor');
+    }
+    res.end();
+}).listen(8080, () => {
+    console.log('Servidor en ejecución en el puerto 8080');
+});
